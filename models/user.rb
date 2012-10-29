bbfb8af7679eb14ef90f7705af48fcc9efe0c086
Ricardo Mendes <rokusu@gmail.com>
2012-10-29 02:41:27 +0000

    improving the sequel-fu

--- models/user.rb
+++ models/user.rb
@@ -5,14 +5,17 @@ module Cyberscore::Model
   class User < Sequel::Model
     one_to_many :records
 
+    def newest_records(this_much=10)
+      @newest_records ||= records.reverse.first this_much
+    end
+
     def medals
       medals = OpenStruct.new
       medals.platinum = Record.where(:user_id => self.pk).where(:platinum => 1).count
 
-      top_3 = Record.
-        join(:games, :game_id => :game_id).
-        where(:user_id => self.pk).where('chart_pos <= 3').where(:site_id => 1).
-        group_and_count(:chart_pos).all.map do |it|
+      top_3 = Record.join(:games, :game_id => :game_id)              \
+        .where([user_id: self.pk], ['chart_pos <= 3'], [site_id: 1]) \
+        .group_and_count(:chart_pos).all.map do |it|
           it.values[:count]
         end
 
