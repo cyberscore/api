f840ea347686e13d38741a36f9b47a27a0622798
Ricardo Mendes <rokusu@gmail.com>
2012-10-29 03:27:35 +0000

    smooth operator~ smooth~ operator~

--- models/user.rb
+++ models/user.rb
@@ -10,18 +10,17 @@ module Cyberscore::Model
     end
 
     def medals
-      medals = OpenStruct.new
-      medals.platinum = Record.where(:user_id => self.pk).where(:platinum => 1).count
+      Struct.new(:platinum, :gold, :silver, :bronze).new.tap do |medals|
+        medals.platinum = Record.where([user_id: self.pk], [platinum: 1]).count
 
-      top_3 = Record.join(:games, :game_id => :game_id)              \
-        .where([user_id: self.pk], ['chart_pos <= 3'], [site_id: 1]) \
-        .group_and_count(:chart_pos).all.map do |it|
-          it.values[:count]
-        end
+        top_3 = Record.join(:games, :game_id => :game_id)              \
+          .where([user_id: self.pk], ['chart_pos <= 3'], [site_id: 1]) \
+          .group_and_count(:chart_pos).all.map do |it|
+            it.values[:count]
+          end
 
-      medals.gold, medals.silver, medals.bronze = top_3.fill 0, top_3.length...3
-
-      medals
+        medals.gold, medals.silver, medals.bronze = top_3.fill 0, top_3.length...3
+      end
     end
   end
 
