af8e88022d181e1f92c83ac7baa62edc16d14d10
Ricardo Mendes <rokusu@gmail.com>
2013-02-09 18:55:45 +0000

    autoload all the stuffs!

--- representers.rb
+++ representers.rb
@@ -7,20 +7,23 @@ module Cyberscore::Representer
   module Relations
     include Roar::Representer::JSON::HAL
 
-    link rel:     :curie,
-         name:       :cs,
+    link rel:       :curie,
+         name:      :cs,
          templated: true do "/rels/{rel}" end
   end
 
 end
 
-require_relative 'representers/cyberscore'
-require_relative 'representers/feed'
-require_relative 'representers/game'
-require_relative 'representers/medals'
-require_relative 'representers/news'
-require_relative 'representers/record'
-require_relative 'representers/score'
-require_relative 'representers/submission'
-require_relative 'representers/user'
-require_relative 'representers/notification'
+module Cyberscore::Representer
+  autoload :Error,        './representers/error'
+  autoload :Root,         './representers/cyberscore'
+  autoload :Feed,         './representers/feed'
+  autoload :Game,         './representers/game'
+  autoload :Medals,       './representers/medals'
+  autoload :News,         './representers/news'
+  autoload :Record,       './representers/record'
+  autoload :Score,        './representers/score'
+  autoload :Submission,   './representers/submission'
+  autoload :User,         './representers/user'
+  autoload :Notification, './representers/notification'
+end
